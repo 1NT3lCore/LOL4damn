16c9c90cd3df74e219a22974b39461e32612beba
1NT3lCore <kpanafidenko@mail.ru>
2024-04-08 21:03:01 +0300

    YO

---
+++ LOL4.2/LOL4.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int SumRange(int A, int B) {
+    if (A > B)
+        return 0;
+    int sum = 0;
+    for (int i = A; i <= B; ++i)
+        sum += i;
+    return sum;
+}
+
+int main() {
+    int A, B, C;
+    cout << "Enter A: ";
+    while (!(cin >> A)) {
+        cout << "Incorrect input. Please try again: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Enter B: ";
+    while (!(cin >> B)) {
+        cout << "Incorrect input. Please try again: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Enter C: ";
+    while (!(cin >> C)) {
+        cout << "Incorrect input. Please try again: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Sum from A to B is " << SumRange(A, B) << '\n';
+    cout << "Sum from B to C is " << SumRange(B, C) << '\n';
+    return 0;
+}
---
+++ LOL4.3/LOL4.3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int SumDigits(int N) {
+    if (N == 0)
+        return 0;
+    return N % 10 + SumDigits(N / 10);
+}
+
+int main() {
+    int N;
+    cout << "Enter a positive integer: ";
+    while (!(cin >> N) || N <= 0) {
+        cout << "Incorrect input. Please try again: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Sum of digits: " << SumDigits(N) << '\n';
+    return 0;
+}
